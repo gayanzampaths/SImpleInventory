73921843ed0d702a75aa25c4b06da6f68967d1f0
Gayan Sampath <gayansampath@ymail.com>
2018-08-26 23:52:37 +0530

    Bootstrapping SpringBoot

---
+++ src/main/java/spring/gayan/MiddleCube/SimpleInventoryApp.java
@@ -0,0 +1,15 @@
+package spring.gayan.MiddleCube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//annotation for spring
+@SpringBootApplication
+public class SimpleInventoryApp {
+
+	public static void main(String[] args) {
+		//Bootstrapping SpringBoot 
+		SpringApplication.run(SimpleInventoryApp.class, args);
+	}
+
+}
